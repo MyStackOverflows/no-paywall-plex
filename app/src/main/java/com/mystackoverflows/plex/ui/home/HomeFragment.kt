1076c57c3ff997131ad45bad077784512b518786
MyStackOverflows <piunger@hotmail.com>
2024-03-15 00:02:14 -0700

    yes

--- app/src/main/java/com/mystackoverflows/plex/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mystackoverflows/plex/ui/home/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.mystackoverflows.plex.R
 import com.mystackoverflows.plex.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
