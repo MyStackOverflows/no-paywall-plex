fa7c1625ef5154e448f249ee2f14572f566c2d45
MyStackOverflows <piunger@hotmail.com>
2024-03-15 22:19:12 -0700

    yes

--- app/src/main/java/com/mystackoverflows/plex/TestingFragment.kt
+++ app/src/main/java/com/mystackoverflows/plex/TestingFragment.kt
@@ -17,16 +17,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TestingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -51,8 +44,6 @@ class TestingFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             TestingFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
